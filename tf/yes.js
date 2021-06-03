9890e1cc5e764a2cd99c0477e20c66e3055efa96
Emma Viking <emmvik021011@student.jenseneducation.se>
2021-06-03 09:08:49 +0200

    Add files via upload

--- tf/yes.js
+++ tf/yes.js
@@ -4,3 +4,31 @@ element.addEventListener("click", toggleDoor);
 function toggleDoor() {
   element.classList.toggle("doorOpen");
 }
+
+var element2 = document.querySelector(".door2");
+element2.addEventListener("click", toggleDoor2);
+
+function toggleDoor2() {
+  element2.classList.toggle("doorOpen2");
+}
+
+var element3 = document.querySelector(".door3");
+element3.addEventListener("click", toggleDoor3);
+
+function toggleDoor3() {
+  element3.classList.toggle("doorOpen3");
+}
+
+var element4 = document.querySelector(".door4");
+element4.addEventListener("click", toggleDoor4);
+
+function toggleDoor4() {
+  element4.classList.toggle("doorOpen4");
+}
+
+var element5 = document.querySelector(".door5");
+element5.addEventListener("click", toggleDoor5);
+
+function toggleDoor5() {
+  element5.classList.toggle("doorOpen5");
+}
